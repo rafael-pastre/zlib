3bdc8d965201a63a046918929b1e8561d2e0833c
Rafael Pastre <rafael.pastre@usp.br>
2019-11-13 19:59:34 -0300

    new structures

---
+++ data_structures/graph/zgraph.h
@@ -0,0 +1,11 @@
+#ifndef LIB_ZENITH_GRAPH
+#define LIB_ZENITH_GRAPH
+
+/* Data Structures */
+typedef struct _graph
+{
+	// Graph type
+	// Adj matrix
+	// Adj list
+}Graph;
+#endif//LIB_ZENITH_GRAPH
---
+++ data_structures/linked_list/linked_list.h
@@ -0,0 +1,17 @@
+#ifndef LIB_ZENITH_LINKED_LIST
+#define LIB_ZENITH_LINKED_LIST
+
+/* Data Structures */
+typedef struct _zllist_node_
+{
+	
+}zLListNode;
+
+typedef struct _zlinked_list
+{
+	// elememt type
+	// first node
+	// last node
+	// comparation function
+}zLinkedList;
+#endif//LIB_ZENITH_LINKED_LIST
